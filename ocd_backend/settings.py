de1cecc00631c1b27266cdf58c79341b140d8703
Breyten Ernsting <bje@dds.nl>
2016-08-15 18:06:44 +0200

    More replacements of ori with duo and removing stuff we do not need.

--- ocd_backend/settings.py
+++ ocd_backend/settings.py
@@ -63,32 +63,22 @@ TEMP_DIR_PATH = os.path.join(ROOT_PATH, 'temp')
 SOURCES_CONFIG_FILE = os.path.join(ROOT_PATH, 'sources.json')
 
 # The name of the index containing documents from all sources
-COMBINED_INDEX = 'ori_combined_index'
+COMBINED_INDEX = 'duo_combined_index'
 
 # The default prefix used for all data
-DEFAULT_INDEX_PREFIX = 'ori'
+DEFAULT_INDEX_PREFIX = 'duo'
 
 RESOLVER_BASE_URL = 'http://localhost:5000/v0/resolve'
-RESOLVER_URL_INDEX = 'ori_resolver'
+RESOLVER_URL_INDEX = 'duo_resolver'
 
 # The User-Agent that is used when retrieving data from external sources
-USER_AGENT = 'Open Raadsinformatie/0.1 (+http://www.openraadsinformatie.nl/)'
+USER_AGENT = 'DUO API/0.1 (+http://www.duo.nl/)'
 
 # URL where of the API instance that should be used for management commands
 # Should include API version and a trailing slash.
 # Can be overridden in the CLI when required, for instance when the user wants
 # to download dumps from another API instance than the one hosted by OpenState
 API_URL = 'http://127.0.0.1:5000/v0/'
-
-# The endpoint for the iBabs API
-IBABS_WSDL = u'https://www.mijnbabs.nl/iBabsWCFService/Public.svc?singleWsdl'
-
-# The endpoint for the CompanyWebcast API
-CWC_WSDL = u'https://services.companywebcast.com/meta/1.2/metaservice.svc?singleWsdl'
-
-# define the location of pdftotext
-PDF_TO_TEXT = u'pdftotext'
-PDF_MAX_MEDIABOX_PIXELS = 5000000
 
 # Allow any settings to be defined in local_settings.py which should be
 # ignored in your version control system allowing for settings to be
