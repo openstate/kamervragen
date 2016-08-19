55adac64d89fd933618a7fc2931c6786105525fd
Breyten Ernsting <bje@dds.nl>
2016-08-19 12:28:21 +0200

    Factor out a common thing which we can later use in the extractor that reads and imports the downloaded CSVs.

--- ocd_backend/extractors/duo.py
+++ ocd_backend/extractors/duo.py
@@ -11,20 +11,30 @@ from ocd_backend.utils.misc import make_hash_filename, download_file
 from ocd_backend import settings
 
 
-class DownloadExtractor(BaseExtractor, HttpRequestMixin):
+class CSVExtractor(BaseExtractor):
+    """
+    Add a mathod to read a CSV file. Yields rows as dicts.
+    """
+    def _read(self):
+        with open(self.source_definition['csv_file']) as csvfile:
+            reader = UnicodeReaderAsDict(csvfile)
+            for row in reader:
+                yield row
+
+
+class DownloadExtractor(CSVExtractor):
     """
     Download files listed in a given CSV file. The extractor only downloads
     files, but does not yield items, as there is no need to store any data.
     """
     def run(self):
-        with open(self.source_definition['csv_file']) as csvfile:
-            reader = UnicodeReaderAsDict(csvfile)
-            for row in reader:
-                #pprint(row)
-                url = row[self.source_definition['csv_url_field']]
-                local_filename = os.path.join(
-                    self.source_definition['csv_download_path'],
-                    make_hash_filename(url)
-                )
-                download_file(url, local_filename)
+        for row in self._read():
+            url = row[self.source_definition['csv_url_field']]
+            print url
+            local_filename = os.path.join(
+                self.source_definition['csv_download_path'],
+                make_hash_filename(url)
+            )
+            # FIXME: does not actually use the request mixin right now
+            download_file(url, local_filename)
         return []
