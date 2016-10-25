c48c37577bfce2744dd3b673f0aa6097efbe6922
Sicco van Sas <sicco@openstate.eu>
2016-10-26 00:10:47 +0200

    Log download and update messages

--- bin/download_and_update_duo_api_datasets.sh
+++ bin/download_and_update_duo_api_datasets.sh
@@ -5,7 +5,9 @@ cd /opt/duo
 source ../bin/activate
 
 # Download and update datasets
-./manage.py extract start download
-./manage.py extract start update
+echo `date`: starting new download >> log/download_update.log
+./manage.py extract start download >> log/download_update.log 2>&1
+echo `date`: starting new update >> log/download_update.log
+./manage.py extract start update >> log/download_update.log 2>&1
 sleep 60
 ./bin/fix_aliases.py
