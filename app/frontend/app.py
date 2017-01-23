946aa5b5f90c5bdff4c52623b5ff0307e5f74c7f
Breyten Ernsting <bje@dds.nl>
2017-01-23 17:51:46 +0100

    Make changes so we can do http requests via the api layer.

--- app/frontend/app.py
+++ app/frontend/app.py
@@ -3,18 +3,30 @@ import simplejson as json
 from flask import (
     Flask, abort, jsonify, request, redirect, render_template,
     stream_with_context, Response)
+import requests
+
 
 app = Flask(__name__)
 
 
+class BackendAPI(object):
+    URL = 'http://c-tkv-nginx/v0'
+
+    def sources(self):
+        return requests.get('%s/sources' % (self.URL,)).json()
+
+api = BackendAPI()
+
+
 @app.route("/")
 def main():
-    return render_template('index.html')
+    return render_template('index.html', sources=None)
 
 
 @app.route("/zoeken")
 def search():
-    return render_template('index.html')
+    sources = api.sources()
+    return render_template('index.html', sources=sources)
 
 
 def create_app():
