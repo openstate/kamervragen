d5a08ce28153dd9b2199271e8fa65f209ab92192
Breyten Ernsting <bje@dds.nl>
2017-03-21 16:45:30 +0100

    Slight rewrite

--- ocd_backend/extractors/api.py
+++ ocd_backend/extractors/api.py
@@ -14,14 +14,20 @@ class FrontendAPIExtractor(BaseExtractor, HttpRequestMixin, FrontendAPIMixin):
     Extracts items from the frontend API.
     """
     def run(self):
-        index_name = (self.source_definition.get('frontend_index') or
+        index_name = (
+            self.source_definition.get('frontend_index') or
             self.source_definition['index_name'])
-        print "Searching in %s ..." % (index_name,)
-        results = self.api_request(index_name,
-            self.source_definition['frontend_type'],
-            **self.source_definition['frontend_args'])  # 100 for now ...
-        for result in results[u'hits'][u'hits']:
-            print "%s - %s" % (result['name'], result['date'],)
-            #print u"%s - %s (%s)" % (result['start_date'], result['name'], result['id'],)
-            #pprint(result)
-            yield 'application/json', json.dumps(result)
+        api_args = self.source_definition['frontend_args']
+        api_results = 1
+        api_page = 1
+        # TODO: implement actual paging .... ;) (let's)
+        while api_results > 0:
+            print "Fetching page %s ..." % (api_page,)
+            results = self.api_request(
+                index_name,
+                self.source_definition['frontend_type'],
+                **api_args)   # 100 for now ...
+            for result in results[u'hits'][u'hits']:
+                print "%s - %s" % (result['name'], result['date'],)
+                yield 'application/json', json.dumps(result)
+            api_results = 0
